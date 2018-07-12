1ab0d504fc519c8de5134ca4fb438356ad6c9518
Alex <alexjosef@outlook.com>
2018-07-12 08:39:43 -0300

    MediaIdade

---
+++ T1/MediaIdade/MediaIdade/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaIdade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idadeLucas = 20;
+            int idadeBryan = 21;
+            int idadeFelipe = 17;
+
+            double mediaidade = (idadeBryan + idadeFelipe + idadeLucas) / 3;
+            MessageBox.Show("A média das idades entre eles é:" + mediaidade);
+        }
+    }
+}

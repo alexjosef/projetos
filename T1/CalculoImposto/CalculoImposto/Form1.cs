7d964759974fe5266d903f9141d035bd6e73f006
Alex <alexjosef@outlook.com>
2018-07-12 11:42:43 -0300

    CalculoImposto

---
+++ T1/CalculoImposto/CalculoImposto/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoImposto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valorDaNotaFiscal = 3999;
+            double imposto;
+
+            if (valorDaNotaFiscal < 1000)
+            {
+                imposto = 0.02;
+            }
+
+            else if (valorDaNotaFiscal < 3000)
+            {
+                imposto = 0.025;
+            }
+
+            else if (valorDaNotaFiscal < 7000)
+            {
+                imposto = 0.028;
+            }
+
+            else 
+            {
+                imposto = 0.03;
+            }
+            MessageBox.Show("O valor de imposto séra:" + imposto);
+        }   
+
+    }
+}

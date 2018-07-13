1389e50311e61c86c9566f36f35a81722a429fb2
Alex <alexjosef@outlook.com>
2018-07-13 11:32:09 -0300

    Testando criação de conta com class

---
+++ T1/TesteConta/TesteConta/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConta
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+        public int agencia;
+        public string CPF;
+    }
+}
---
+++ T1/TesteConta/TesteConta/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteConta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta c = new Conta();
+            c.numero = 1;
+            c.titular = "Alex José";
+            c.saldo = 500.0;
+            c.agencia = 20;
+            c.CPF = "074.242.589.40";
+
+            Conta c2 = new Conta();
+            c2.numero = 2;
+            c2.titular = "Lucas Lopes";
+            c2.saldo = 1000.0;
+            c2.agencia = 10;
+            c2.CPF = "111.444.555.40";
+
+            MessageBox.Show(c.titular + " - " + c.CPF + " - " + c.agencia);
+            MessageBox.Show(c2.titular + " - " + c2.CPF + " - " + c2.agencia);
+            
+        }
+    }
+}

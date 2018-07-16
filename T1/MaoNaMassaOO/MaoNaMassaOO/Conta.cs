d211046c61ec924219f57ab1f763912d885f52ee
Alex <alexjosef@outlook.com>
2018-07-16 10:02:54 -0300

    Mãos na Massa

---
+++ T1/MaoNaMassaOO/MaoNaMassaOO/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaOO
+{
+    class Conta
+    {
+        public int numero;
+        public double saldo;
+        public Cliente titular;
+
+
+        public bool Saca(double ValorASacar)
+        {
+
+            if (ValorASacar < 0 && ValorASacar > this.saldo)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.titular.EhMaiorDeIdade())
+                {
+                    this.saldo -= ValorASacar;
+                    return true;
+                }
+                else
+                {
+                    if (ValorASacar <= 200)
+                    {
+                        this.saldo -= ValorASacar;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+
+                }
+
+            }  
+            
+        }
+       
+    }
+}
---
+++ T1/MaoNaMassaOO/MaoNaMassaOO/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaoNaMassaOO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta ContaAlex = new Conta();
+            ContaAlex.numero = 1;
+            ContaAlex.saldo = 1000.0;
+
+            Cliente ClienteAlex = new Cliente();
+            ClienteAlex.nome = "Alex José";
+            ClienteAlex.idade = 17;
+
+            ContaAlex.titular = ClienteAlex;
+
+            bool sacou = ContaAlex.Saca(200.0);
+            if (sacou)
+            {
+                MessageBox.Show("Saldo da conta após saque: " + ContaAlex.saldo);
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível sacar da conta");
+            }
+
+        }
+    }
+}

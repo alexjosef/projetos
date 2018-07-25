d7e011a9e400515523f24dfb955fcaeb8cd8fef0
Alex <alexjosef@outlook.com>
2018-07-25 11:59:28 -0300

    EF - Migration

--- T2/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T2/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,41 +11,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-
-                var produtos = contexto.Produtos.ToList();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var novoProduto = new Produto
-                {
-                    Nome = "Sabão em Pó",
-                    Categoria = "Limpeza",
-                    Preco = 5.99,
-                };
-                contexto.Produtos.Add(novoProduto);
-
-                contexto.Produtos.Remove(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.SaveChanges();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine("\n\n" + entry.Entity.ToString() + " - " + entry.State);
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("===================");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
+            
         }
     }
 }

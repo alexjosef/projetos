fd9be568e76ba245100437896e7253d867c58231
Alex <alexjosef@outlook.com>
2018-08-06 11:57:21 -0300

    Design Patterns C# 1 - Strategy

---
+++ T2/Curso Design Patterns C# I/DesignPatterns01/DesignPatterns01/ICMS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns01
+{
+    public class ICMS
+    {
+        public double CalculaICMS(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.1;
+        }
+    }
+}
---
+++ T2/Curso Design Patterns C# I/DesignPatterns01/DesignPatterns01/ISS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns01
+{
+    public class ISS
+    {
+        public double CalculaISS(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+    }
+}

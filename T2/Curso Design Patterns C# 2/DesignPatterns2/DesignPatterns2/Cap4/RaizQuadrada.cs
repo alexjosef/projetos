a1393baabc54ba34e405222ccfac703fbf2cc31c
Alex <alexjosef@outlook.com>
2018-08-13 11:55:41 -0300

    Design Patterns C#2 - Interpreter

---
+++ T2/Curso Design Patterns C# 2/DesignPatterns2/DesignPatterns2/Cap4/RaizQuadrada.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Cap4
+{
+    class RaizQuadrada : IExpressao
+    {
+        private IExpressao expressao;
+
+        public RaizQuadrada(IExpressao e)
+        {
+            this.expressao = e;
+        }
+
+        public int Avalia()
+        {
+            return (int)Math.Sqrt(expressao.Avalia());
+        }
+    }
+}
--- T2/Curso Design Patterns C# 2/DesignPatterns2/DesignPatterns2/Program.cs
+++ T2/Curso Design Patterns C# 2/DesignPatterns2/DesignPatterns2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DesignPatterns2.Cap1;
 using DesignPatterns2.Cap2;
 using DesignPatterns2.Cap3;
+using DesignPatterns2.Cap4;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -15,17 +16,14 @@ namespace DesignPatterns2
     {
         static void Main(string[] args)
         {
-            Historico historico = new Historico();
-            Contrato c = new Contrato(DateTime.Now, "Alex", TipoContrato.Novo);
-            historico.Adiciona(c.SalvaEstado());
+            // (1 + 10) + (20 - 10)
+            IExpressao esquerda = new Soma(new Numero(1), new Numero(10));
+            IExpressao direita = new Subtracao(new Numero(20), new Numero(10));
+            IExpressao soma = new Multiplicacao(esquerda, direita);
+            IExpressao e = new RaizQuadrada(soma);
 
-            c.Avanca();
-            historico.Adiciona(c.SalvaEstado());
+            Console.WriteLine(e.Avalia());
 
-            c.Avanca();
-            historico.Adiciona(c.SalvaEstado());
-
-            Console.WriteLine(historico.Pega(2).Contrato.Tipo);
         }
     }
 }

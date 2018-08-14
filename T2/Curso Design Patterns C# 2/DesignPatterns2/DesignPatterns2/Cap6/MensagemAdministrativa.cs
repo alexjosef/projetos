db50b8c1590a750b3e71ebfa1972af5cd9d9f9ad
Alex <alexjosef@outlook.com>
2018-08-14 11:34:24 -0300

    Design Patterns C#2 - Bridges

---
+++ T2/Curso Design Patterns C# 2/DesignPatterns2/DesignPatterns2/Cap6/MensagemAdministrativa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Cap6
+{
+    class MensagemAdministrativa : IMensagem
+    {
+        private string nome;
+
+        public IEnviador Enviador { get; set; }
+
+        public MensagemAdministrativa(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void Envia()
+        {
+            this.Enviador.Envia(this);
+        }
+
+        public string Formata()
+        {
+            return string.Format("Enviando mensagem para o administrador {0}", nome);
+        }
+    }
+}
--- T2/Curso Design Patterns C# 2/DesignPatterns2/DesignPatterns2/Program.cs
+++ T2/Curso Design Patterns C# 2/DesignPatterns2/DesignPatterns2/Program.cs
@@ -3,6 +3,7 @@ using DesignPatterns2.Cap2;
 using DesignPatterns2.Cap3;
 using DesignPatterns2.Cap4;
 using DesignPatterns2.Cap5;
+using DesignPatterns2.Cap6;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -19,15 +20,20 @@ namespace DesignPatterns2
         {
             //CAP4
             //// (1 + 10) + (20 - 10)
-            IExpressao esquerda = new Soma(new Numero(1), new Numero(10));
-            IExpressao direita = new Subtracao(new Numero(20), new Numero(10));
-            IExpressao soma = new Soma(esquerda, direita);
+            //IExpressao esquerda = new Soma(new Numero(1), new Numero(10));
+            //IExpressao direita = new Subtracao(new Numero(20), new Numero(10));
+            //IExpressao soma = new Soma(esquerda, direita);
             //IExpressao e = new RaizQuadrada(soma);
 
-            Console.WriteLine(soma.Avalia());
+            //Console.WriteLine(soma.Avalia());
 
-            PreFixaVisitor prefixa = new PreFixaVisitor();
-            soma.Aceita(prefixa);
+            //PreFixaVisitor prefixa = new PreFixaVisitor();
+            //soma.Aceita(prefixa);
+
+            IEnviador enviador = new EnviaPorEmail();
+            IMensagem mensagem = new MensagemAdministrativa("Alex");
+            mensagem.Enviador = enviador;
+            mensagem.Envia();
 
         }
     }

f983a13440224063166cedd11d990b0276127105
Alex <alexjosef@outlook.com>
2018-08-16 11:12:07 -0300

    SOLID C# - OCP e DIP

---
+++ T2/SOLID C#/MaoNaMassaOCP/MaoNaMassaOCP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaOCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compra compra = new Compra(500, "SAO PAULO");
+            CalculadoraDePrecos calculadora = new CalculadoraDePrecos(new TabelaDePrecoPadrao(), new Frete());
+
+            double resultado = calculadora.Calcula(compra);
+
+            Console.WriteLine("O preço da compra é: {0} ", resultado);
+            Console.ReadKey();
+        }
+    }
+}

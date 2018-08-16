c9313dac6a35685f7b6e03ef8a9f32ef548ba244
Alex <alexjosef@outlook.com>
2018-08-16 09:53:15 -0300

    SOLID C# - Mão na Massa Acoplamento

---
+++ T2/SOLID C#/MaoNaMassaAcoplamento/MaoNaMassaAcoplamento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaAcoplamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnviadorDeEmail enviadorEmail = new EnviadorDeEmail();
+            NotaFiscalDao nfDao = new NotaFiscalDao();
+            IList<IAcaoAposGerarNota> acoes = new List<IAcaoAposGerarNota>();
+            acoes.Add(enviadorEmail);
+            acoes.Add(nfDao);
+
+            GeradorDeNotaFiscal gnf = new GeradorDeNotaFiscal(acoes);
+            Fatura fatura = new Fatura(200, "Alex");
+            gnf.Gera(fatura);
+
+            Console.ReadKey();
+
+        }
+    }
+}

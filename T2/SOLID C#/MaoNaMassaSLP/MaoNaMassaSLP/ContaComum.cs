82587446c0f0e47959a15e35d21df5c279fa661b
Alex <alexjosef@outlook.com>
2018-08-17 11:46:17 -0300

    SOLID C# - Mão na Massa SLP

---
+++ T2/SOLID C#/MaoNaMassaSLP/MaoNaMassaSLP/ContaComum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaSLP
+{
+    public class ContaComum
+    {
+        public double Saldo { get; private set; }
+
+        private ManipuladorDeSaldo manipulador;
+        public ContaComum()
+        {
+            this.manipulador = new ManipuladorDeSaldo();
+        }
+
+        public void Saca(double valor)
+        {
+            manipulador.Saca(valor - 100);
+        }
+
+        public void Deposita(double valor)
+        {
+            manipulador.Deposita(valor);
+        }
+
+        public void SomaInvestimento()
+        {
+            this.manipulador.SomaInvestimento(1.1);
+        }
+    }
+}
---
+++ T2/SOLID C#/MaoNaMassaSLP/MaoNaMassaSLP/ContaEstudante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaSLP
+{
+    public class ContaDeEstudante : ContaComum
+    {
+        private ManipuladorDeSaldo m;
+
+        public int Milhas { get; private set; }
+
+        public ContaDeEstudante()
+        {
+            m = new ManipuladorDeSaldo();
+        }
+        public new void Deposita(double valor)
+        {
+            m.Deposita(valor);
+            this.Milhas += (int)valor;
+        }
+    }
+}

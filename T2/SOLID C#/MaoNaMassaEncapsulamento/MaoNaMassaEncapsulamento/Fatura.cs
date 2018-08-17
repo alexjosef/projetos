8a1c41da54d4cadcde51fff38ab14689fcece04f
Alex <alexjosef@outlook.com>
2018-08-17 09:22:13 -0300

    SOLID C# - Encapsulamento

---
+++ T2/SOLID C#/MaoNaMassaEncapsulamento/MaoNaMassaEncapsulamento/Fatura.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaEncapsulamento
+{
+    public class Fatura
+    {
+        public string Cliente { get; private set; }
+        public double Valor { get; set; }
+        public List<Pagamento> Pagamentos { get; private set; }
+        public bool Pago { get; private set; }
+
+        public Fatura(string cliente, double valor)
+        {
+            this.Cliente = cliente;
+            this.Valor = valor;
+            this.Pagamentos = new List<Pagamento>();
+            this.Pago = false;
+        }
+        public void AdicionaPagamento(Pagamento pagamento)
+        {
+            this.Pagamentos.Add(pagamento);
+
+            if(valorTotalDosPagamentos() >= Valor)
+            {
+                this.Pago = true;
+            }
+        }
+        private double valorTotalDosPagamentos()
+        {
+            double total = 0;
+
+            foreach(Pagamento pag in Pagamentos)
+            {
+                total += pag.Valor;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ T2/SOLID C#/MaoNaMassaEncapsulamento/MaoNaMassaEncapsulamento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaoNaMassaEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Boleto> boletos = new List<Boleto>();
+            boletos.Add(new Boleto(200));
+            boletos.Add(new Boleto(300));
+            boletos.Add(new Boleto(400));
+
+            Fatura fatura = new Fatura("Caelum", 870);
+
+            ProcessadorDeBoletos pdb = new ProcessadorDeBoletos();
+
+            pdb.Processa(boletos, fatura);
+
+            Console.WriteLine(fatura.Pago);
+            Console.ReadKey();
+        }
+    }
+}

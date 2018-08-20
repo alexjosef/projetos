25c089b8bf524e6cb0ccf4f66974f166353f57d9
Alex <alexjosef@outlook.com>
2018-08-20 10:06:07 -0300

    TDD - Primeiro Teste Leilao

---
+++ T2/TDD/Caelum.Leilao/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    class Avaliador
+    {
+        private double maiorValor = double.MinValue;
+        private double menorValor = double.MaxValue;
+
+        public void Avalia(Leilao leilao)
+        {
+            foreach(var lance in leilao.Lances)
+            {
+                if (lance.Valor > maiorValor)
+                {
+                    maiorValor = lance.Valor;
+                }
+
+                if (lance.Valor < menorValor)
+                {
+                    menorValor = lance.Valor;
+                }
+            }
+        }
+
+        public double maiorLance
+        {
+            get { return maiorValor; }
+        }
+
+        public double menorLance
+        {
+            get { return menorValor; }
+        }
+    }
+}

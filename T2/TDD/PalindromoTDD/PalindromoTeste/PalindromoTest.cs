c571371ece77deaed63e5c90820e3a582b79ed5a
Alex <alexjosef@outlook.com>
2018-08-20 11:26:52 -0300

    TDD - Palindromo

---
+++ T2/TDD/PalindromoTDD/PalindromoTeste/PalindromoTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PalindromoTDD;
+
+namespace PalindromoTeste
+{
+    [TestFixture]
+    public class PalindromoTest
+    {
+        [Test]
+        public void DeveIdentificarPalindromoEFiltrarCaracteresInvalidos()
+        {
+            Palindromo p = new Palindromo();
+
+            bool resultado = p.EhPalindromo(
+                "Socorram-me subi no onibus em Marrocos");
+            Assert.IsTrue(resultado);
+        }
+
+        [Test]
+        public void DeveIdentificarPalindromo()
+        {
+            Palindromo p = new Palindromo();
+
+            bool resultado = p.EhPalindromo(
+                "Anotaram a data da maratona");
+            Assert.IsTrue(resultado);
+        }
+
+        [Test]
+        public void DeveIdentificarSeNaoEhPalindromo()
+        {
+            Palindromo p = new Palindromo();
+
+            bool resultado = p.EhPalindromo(
+                "E preciso amar as pessoas como se nao houvesse amanha");
+            Assert.IsFalse(resultado);
+        }
+
+    }
+}

d22e048edea7cba71e25d87802ffdfac08fd834a
Alex <alexjosef@outlook.com>
2018-09-13 11:10:23 -0300

    JQuery Módulo 2 Finalizado

--- T3/JQuery/alura-typer/public/js/placar.js
+++ T3/JQuery/alura-typer/public/js/placar.js
@@ -1,8 +1,9 @@
 $("#botao-placar").click(mostraPlacar);
+$("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
   var corpoTabela = $(".placar").find("tbody");
-  var usuario = "Alex";
+  var usuario = $("#usuarios").val();
   var numPalavras = $("#contador-palavras").text();
 
   var linha = novaLinha(usuario, numPalavras);
@@ -53,4 +54,45 @@ function scrollPlacar() {
   $("body").animate({
     scrollTop: posicaoPlacar + "px"
   },1000);
+}
+
+function sincronizaPlacar() {
+  var placar = [];
+  var linhas = $("tbody>tr");
+
+  linhas.each(function () {
+    var usuario = $(this).find("td:nth-child(1)").text();
+    var palavras = $(this).find("td:nth-child(2)").text();
+
+    var score = {
+      usuario: usuario,
+      pontos: palavras
+    };
+      placar.push(score);
+  });
+    var dados = {
+      placar: placar
+    };
+      $.post("http://localhost:3000/placar", dados , function() {
+      console.log("Placar sincronizado com sucesso");
+      $(".tooltip").tooltipster("open");
+  }).fail(function(){
+      $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar");
+  }).always(function(){
+      setTimeout(function() {
+      $(".tooltip").tooltipster("close");
+  }, 1200);
+  });
+}
+
+function atualizaPlacar(){
+    $.get("http://localhost:3000/placar",function(data){
+        $(data).each(function(){
+            var linha = novaLinha(this.usuario, this.pontos);
+
+            linha.find(".botao-remover").click(removeLinha);
+
+            $("tbody").append(linha);
+        });
+    });
 }
